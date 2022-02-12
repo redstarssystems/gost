6dee28bfc771a9fc4c2e17ec34f66fae32807036
Mike Ananev <mike1452@yandex.ru>
2022-02-12 09:49:49 +0300

    fix doc

--- dev/src/examples/encryption.clj
+++ dev/src/examples/encryption.clj
@@ -11,17 +11,17 @@
 
 ;; To generate a secret key GOST28147-89 use `generate-secret-key` function with a parameter.
 ;; This will return a 256-bit random secret key as a SecretKeySpec object.
-;; The algorithm is set GOST28147
+;; The algorithm is set to GOST28147.
 (def secret-key-89 (e/generate-secret-key e/gost28147))
 (e/algo-name secret-key-89) ;; => GOST28147
 
-;; To convert SecretKeySpec to a byte array
+;; To convert a SecretKeySpec to a byte array:
 (e/secret-key->byte-array secret-key-2015) ;; => [B
 ;; [-38, -86, 71, -42, -69, 73, -33, 53, 72, 80, 38, 26, 57, 69, -114, -1,
 ;; -119, 13, 113, -84, -31, 54, -128, 114, -79, -55, 85, 126, 105, -96,
 ;; -37, -128]
 
-;; To convert a byte array to SecretKeySpec
+;; To convert a byte array to SecretKeySpec:
 (e/byte-array->secret-key (byte-array [-38, -86, 71, -42, -69, 73, -33, 53, 72, 80, 38, 26, 57, 69, -114, -1,
                                        -119, 13, 113, -84, -31, 54, -128, 114, -79, -55, 85, 126, 105, -96,
                                        -37, -128])) ;; => #object[javax.crypto.spec.SecretKeySpec
@@ -50,7 +50,7 @@
 ;; This function calculates Mac for plain data, then
 ;; compress a plain data to hide information structure, then
 ;; encrypts data and Mac in CFB mode with always random IV.
-;; Encrypted bytes from the same message and same key are always different!
+;; The encrypted bytes from the same message and same key are always different!
 (def encrypted-message (e/protect-bytes secret-key-2015 (.getBytes message))) ;; Returns bytes array with structure:
 ;; [random(IV), encrypted(Mac), encrypted(compressed-data)]
 
@@ -89,7 +89,7 @@
 (e/mac-stream secret-key-2015 "dev/src/examples/plain32.txt") ;; => [B
 ;; [-111, 125, 10, -34, -109, -109, 41, 115, 81, 61, -90, -80, 16, 71, -108, 91]
 
-;; To calculate Mac for byte array (any binary file) use the same `mac-stream` function.
+;; To calculate Mac for a byte array (any binary file) use the same `mac-stream` function.
 ;; The encryption algorithm GOST3412-2015 or GOST28147-89 is already set in SecretKeySpec.
 ;; Mac value from the same data and same SecretKeySpec is always the same.
 (e/mac-stream secret-key-2015 (.getBytes message)) ;; => [B
